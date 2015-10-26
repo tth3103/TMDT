4da8d95a4894ff447dea76263dde4a64cca57ae6
hadros <mr.hadros@gmail.com>
2015-10-26 11:27:22 +0700

    Copyrights fixes.

--- templates/page--front.tpl.php
+++ templates/page--front.tpl.php
@@ -226,7 +226,7 @@
       <footer<?php print $footer_attributes; ?>>
         <?php print render($page['footer']); ?>
         <?php print $attribution; ?>
-        <div class="copyright"><p>Drupal DEMO Store. Copyright ©&nbsp;2015 Advanced Themes by <a href="http://adcisolutions.com/" title="drupal development">ADCI solutions</a>. All Rights Reserved.</p></div>
+        <div class="copyright"><p>Drupal DEMO Store. Copyright ©&nbsp;2015 Themes by <a href="http://adcisolutions.com/" title="drupal development">ADCI solutions</a>. All Rights Reserved.</p></div>
       </footer>
     <?php endif; ?>
 
